cfe34e1717aa600c646baee08dea0e867295dd9b
Mardon1207 <mardonsultonov8@gmail.com>
2023-07-28 09:42:18 +0500

    database

--- db.py
+++ db.py
@@ -1,13 +1,19 @@
-from tinydb import TinyDB, Query
+from tinydb import TinyDB,Query
+from tinydb.database import Document
 
 class DB:
     def __init__(self,path):
         self.db = TinyDB(path)
+        
 
     def get_tables(self):
         """
         To get the list of all the tables in the database
         """
+        tables = self.db.tables()
+        t=list(tables)
+        return t
+        
         
     def getPhone(self,brand,idx):
         """
@@ -17,8 +23,17 @@ class DB:
         return:
             dict
         """
+        d=self.db.table(brand).get(doc_id=idx)
+        return d
 
     def get_phone_list(self,brand):
         """
         Return phone list
         """
+        l=[]
+        s=1
+        for i in self.db.table(str(brand)):
+            m=self.db.table(str(brand)).get(doc_id=s)["name"]+f',{s}'
+            s+=1
+            l.append(m)
+        return l
