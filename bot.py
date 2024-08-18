521f5ffef90ab9d89bfbc0bc0825de1bfe29aecd
Mardon1207 <mardonsultonov8@gmail.com>
2024-08-18 20:20:35 +0500

    qwerty

--- bot.py
+++ bot.py
@@ -17,8 +17,7 @@ updater = Updater(TOKEN)
 
 dp = updater.dispatcher
 
-dp.add_handler(MessageHandler(Filters.text("✍️ Test yaratish"), test_yaratish))
-dp.add_handler(MessageHandler(Filters.text("♻️ Orqaga"), bosh_sahifa))
+
 conv_handler0 = ConversationHandler(
     entry_points=[CommandHandler('start', start)],
     states={
@@ -86,17 +85,20 @@ conv_handler7 = ConversationHandler(
     fallbacks=[CommandHandler('cancel', cancel)],
 ) 
 
+dp.add_handler(MessageHandler(Filters.text("✍️ Test yaratish"), test_yaratish))
+dp.add_handler(MessageHandler(Filters.text("♻️ Orqaga"), bosh_sahifa))
 dp.add_handler(MessageHandler(Filters.text("⚙️ Sozlamalar"), sozlanmalar))
 dp.add_handler(MessageHandler(Filters.text("👨‍💻 Admin"), admin))
 dp.add_handler(MessageHandler(Filters.text("📟 Pullik kanallar"), pullik))
-dp.add_handler(conv_handler7)
-dp.add_handler(conv_handler6)
-dp.add_handler(conv_handler5)
-dp.add_handler(conv_handler4)
-dp.add_handler(conv_handler3)
-dp.add_handler(conv_handler2)
-dp.add_handler(conv_handler1)
 dp.add_handler(conv_handler0)
+dp.add_handler(conv_handler1)
+dp.add_handler(conv_handler2)
+dp.add_handler(conv_handler3)
+
+dp.add_handler(conv_handler4)
+dp.add_handler(conv_handler6)
+dp.add_handler(conv_handler7)
+dp.add_handler(conv_handler5)
 
 
 updater.start_polling()
